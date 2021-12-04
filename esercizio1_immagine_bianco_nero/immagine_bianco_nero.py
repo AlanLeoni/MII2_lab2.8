82122df9b579544ed1891ad7368a370082cd4598
Alan <alan@alanleoni.ch>
2021-12-04 14:24:37 +0100

    prima versione es. 1 funzionante

--- esercizio1_immagine_bianco_nero/immagine_bianco_nero.py
+++ esercizio1_immagine_bianco_nero/immagine_bianco_nero.py
@@ -1,5 +1,6 @@
-from img_lib_v0_51 import altezza_immagine, larghezza_immagine, sovrapponi, rettangolo, Immagine
-from testing_util import controlla_valore_atteso
+from sys import float_repr_style
+from img_lib_v0_51 import affianca_verticale, altezza_immagine, immagine_vuota, larghezza_immagine, sovrapponi, rettangolo, Immagine, visualizza_immagine
+from testing_util import affianca_molte, controlla_valore_atteso
 from typing import List
 
 PIXEL_SIZE = 100
@@ -11,13 +12,14 @@ def render_pixel(colore: str) -> Immagine:
 
     :param colore: colore di riempimento dell'immagine
     :returns: un'immagine rappresentante un pixel colorato
-     """ 
+     """      
     return sovrapponi(
         rettangolo(PIXEL_SIZE, PIXEL_SIZE, colore),
         rettangolo(PIXEL_SIZE + 2, PIXEL_SIZE + 2, "grey"))
+    
 
-controlla_valore_atteso(larghezza_immagine(render_pixel("rgb(255,0,0)")), PIXEL_SIZE + 2)
-controlla_valore_atteso(altezza_immagine(render_pixel("rgb(255,0,0)")), PIXEL_SIZE + 2)
+#controlla_valore_atteso(larghezza_immagine(render_pixel("rgb(255,0,0)")), PIXEL_SIZE + 2)
+#controlla_valore_atteso(altezza_immagine(render_pixel("rgb(255,0,0)")), PIXEL_SIZE + 2)
 
 # Mappa di pixel booleani (bianco == True, nero == False)
 bitmap_bianco_nero = [
@@ -28,7 +30,39 @@ bitmap_bianco_nero = [
 
 
 def colore_bianco_nero(pixel: bool) -> str:
-    pass
-
+    bianco = "rgb(255, 255, 255)"
+    nero = "rgb(0, 0, 0)"
+    if pixel == True:
+        return bianco
+    else:
+        return nero
+        
 def render_bitmap_bianco_nero(bitmap: List[List[bool]]) -> Immagine:
-    pass
+    riga_prec = immagine_vuota()
+    for riga in bitmap_bianco_nero:
+        lista_immagini = [
+            render_pixel(colore_bianco_nero(pixel))
+            for pixel in riga
+            ]
+        composizione_riga = affianca_molte(lista_immagini)
+        composizione_immagine = affianca_verticale(riga_prec, composizione_riga)
+        riga_prec = composizione_immagine
+
+    return composizione_immagine
+
+visualizza_immagine(render_bitmap_bianco_nero(bitmap_bianco_nero))
+
+# def render_bitmap_bianco_nero(bitmap: List[List[bool]]) -> Immagine:
+#     riga_prec = immagine_vuota()
+#     riga = immagine_vuota()
+#     for lista in bitmap:
+#         composizione =affianca_verticale(riga_prec, riga)
+#         riga_prec = composizione
+#         # costruisco la lista di immagini a partire dai True e False
+#         lista_immagini = [
+#             render_pixel(elemento)
+#             for elemento in lista
+#             ]
+#         affianca_molte(lista_immagini)
+#     return composizione
+# visualizza_immagine(render_bitmap_bianco_nero(bitmap_bianco_nero))
--- esercizio1_immagine_bianco_nero/testing_util.py
+++ esercizio1_immagine_bianco_nero/testing_util.py
@@ -13,8 +13,8 @@ def controlla_valore_atteso(valore, valore_atteso):
     if valore != valore_atteso:
         print("Test fallito! Il valore attuale", valore, "differisce dal valore atteso", valore_atteso,
               file=sys.stderr)
-    else:
-        print("Test passato")
+    # else:
+    #     print("Test passato")
 
 
 def affianca_molte(immagini: list) -> Image:
